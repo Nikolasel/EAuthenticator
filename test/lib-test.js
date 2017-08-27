3ea60b265bc5db64fced11b2b460ca962b9735de
Nikolas Eller <nikolas.e@mail.de>
2017-08-28 01:17:39 +0200

    Replace code with own implementations

--- test/lib-test.js
+++ test/lib-test.js
@@ -8,6 +8,9 @@ let counter = 0;
 
 
 describe('HOTP Test', function(){
+    it('Check Base32', function () {
+      assert.equal(hotp.b32ToHex("YAA54OXY"), "c001de3af8");
+    });
     it('check DT', function(){
        assert.equal(hotp.dynamicTruncation("1f8698690e02ca16618550ef7f19da8e945b555a"), "50ef7f19", "false");
     });
@@ -23,5 +26,6 @@ describe('TOTP Test', function(){
     it('Complete Test', function(){
         //assert.equal(hotp.getPin(counter), 540854);
         console.log(totp.getPin());
+        console.log(totp.getPinAsString());
     });
 });
