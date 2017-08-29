61246fd7f6b40353c9cec5f5a86a2d9499cedc9c
Nikolas Eller <nikolas.e@mail.de>
2017-08-30 01:06:27 +0200

    Add tests for storage

--- test/storage-test.js
+++ test/storage-test.js
@@ -6,7 +6,15 @@ describe('Storage Test', function() {
     it('Check Storage', function () {
         let storage = new StorageEngine();
         assert.equal(storage.isDataEncrypted(), false);
-        assert.equal(storage.hasAccountData(), true);
-        storage.addAccount({name : "test", secret : "blala"});
+        assert.equal(storage.existsFileInPath(), true);
+        assert.equal(storage.getAllAccounts().length, 1);
+        assert.throws(function() {storage.addAccount({name : "test", secret : "blala"})}, Error, "Account exits");
+        assert.deepEqual(storage.getAccount("test"), {name : "test", secret : "blala"});
+        storage.deleteAccount("test");
+        assert.equal(storage.getAllAccounts().length, 0);
+        let stor2 = new StorageEngine();
+        assert.equal(stor2.existsFileInPath(), true);
+        assert.equal(stor2.getAllAccounts().length, 0);
+        storage.addAccount({name : "test", secret : "blala"})
     });
 });
