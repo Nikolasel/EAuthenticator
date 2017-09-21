9cfa6914ffc71b80df4a00f9c147e5b68b3064cd
Nikolas Eller <nikolas.e@mail.de>
2017-09-21 14:02:27 +0200

    Problems with mdl-dialog

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,15 @@
 let win = require('electron').remote.getCurrentWindow();
 let Storage = require('../../lib/storage');
+let storage = new Storage();
 const path = require('path');
 const url = require('url');
 const remote = require('electron').remote;
 let app = undefined;
 if (remote !== undefined) app = remote.app;
 let TOTP = require('../../lib/totp');
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages': ['corechart']});
+
+let sync = true;
 
 
 function toAdd() {
@@ -20,7 +23,6 @@ function toAdd() {
 
 function showAccounts() {
     let list = document.getElementById("list-of-accounts");
-    let storage = new Storage();
     if (storage.isDataEncrypted()) {
         //Set Password
     }
@@ -45,16 +47,31 @@ function showAccounts() {
             spanPin.className += " list-pin";
             let spanTimer = document.createElement('span');
             spanTimer.className += " list-time";
-            spanTimer.innerHTML = "test";
+            spanTimer.innerHTML = "...";
             listItem.appendChild(spanPin);
             listItem.appendChild(spanTimer);
 
             let spanEnd = document.createElement('span');
             spanEnd.className += " mdl-list__item-secondary-action";
             //TODO Add tooltips <div class="mdl-tooltip" data-mdl-for="tt1"> Follow </div>
-            spanEnd.innerHTML =  '<button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons mdl-color-text--blue-grey-400">create</i></button>' + ' <button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons mdl-color-text--blue-grey-400">delete</i></button>';
+            let btnRename = document.createElement('button');
+            btnRename.className += " mdl-button mdl-js-button mdl-button--icon";
+            btnRename.innerHTML = "<i class=\"material-icons mdl-color-text--blue-grey-400\">create</i>";
+            btnRename.addEventListener("click", function() {
+                renameAccount(name);
+            }, false);
+            let btnDelete = document.createElement('button');
+            btnDelete.className += " mdl-button mdl-js-button mdl-button--icon";
+            btnDelete.innerHTML = "<i class=\"material-icons mdl-color-text--blue-grey-400\">delete</i>";
+            btnDelete.addEventListener("click", function() {
+                deleteAccount(name);
+            }, false);
+            spanEnd.appendChild(btnRename);
+            spanEnd.appendChild(btnDelete);
+
+            //spanEnd.innerHTML = //'<button class="mdl-button mdl-js-button mdl-button--icon" onclick="renameAccount(accounts[i].name)"><i class="material-icons mdl-color-text--blue-grey-400">create</i></button>' + ' ' +
+            //    '<button class="mdl-button mdl-js-button mdl-button--icon"><i class="material-icons mdl-color-text--blue-grey-400">delete</i></button>';
             listItem.appendChild(spanEnd);
-            //listItem.html = '<span class="mdl-list__item-primary-content">' + accounts[i].name + '</span>';
             list.appendChild(listItem);
             list.appendChild(document.createElement('hr'));
             pins.push(spanPin);
@@ -70,10 +87,10 @@ function showAccounts() {
 
 function updateAll(times, accounts, pins) {
     let seconds = getSecUntil30();
-    if(seconds === 30) {
+    if (seconds === 30) {
         updatePin(accounts, pins);
     }
-    for(let i = 0; i < times.length; i++) {
+    for (let i = 0; i < times.length; i++) {
         makeChart(seconds, times[i]);
     }
 }
@@ -89,11 +106,14 @@ function updatePin(accounts, accountsMid) {
 
 function getSecUntil30() {
     let time = new Date();
-    while (time.getMilliseconds() > 100) {
-        time = new Date();
+    if (sync) {
+        while (time.getMilliseconds() > 100) {
+            time = new Date();
+        }
+        sync = false;
     }
     let seconds = time.getSeconds();
-    if(seconds >= 30) {
+    if (seconds >= 30) {
         return (60 - seconds);
     } else {
         return (30 - seconds);
@@ -111,7 +131,7 @@ function makeChart(time, object) {
     let options = {
         legend: 'none',
         pieSliceText: 'none',
-        tooltip: { trigger: 'none' },
+        tooltip: {trigger: 'none'},
         slices: {
             0: {color: 'transparent'},
             1: {color: '#2196f3'}
@@ -120,4 +140,84 @@ function makeChart(time, object) {
     let chart = new google.visualization.PieChart(object);
     chart.draw(data, options);
     //object.innerHTML = time;
+}
+
+function renameAccount(name) {
+    /*let res = prompt("New Name?", "");
+    if(res === "") {
+        alert("Empty");
+        return;
+    }
+    try {
+        storage.renameAccount(name, res);
+        remote.getCurrentWindow().reload();
+    }
+    catch (e) {
+        alert(e.message);
+    }*/
+    let close = document.createElement('button');
+    close.setAttribute('type', 'button');
+    close.className += ' mdl-button color-green';
+    close.innerHTML = "Close";
+    close.addEventListener('click', function () {
+        while (dialog.firstChild) {
+            dialog.removeChild(dialog.firstChild);
+        }
+        dialog.close()
+    }, false);
+    let doSom = document.createElement('button');
+    doSom.setAttribute('type', 'button');
+    doSom.className += ' mdl-button color-green';
+    doSom.innerHTML = "Disabled action";
+    let text = document.createElement('div');
+    text.className += ' mdl-textfield mdl-js-textfield';
+    let input = document.createElement('input');
+    input.className += ' mdl-textfield__input';
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'inputRename');
+    let label = document.createElement('label');
+    label.className += ' mdl-textfield__label';
+    label.setAttribute('for', 'inputRename');
+    label.innerHTML = 'New Account Name';
+    input.innerHTML = name;
+    text.appendChild(input);
+    text.appendChild(label);
+    let array = [close, doSom];
+    makeDialog("Test", text, array);
+}
+
+
+function deleteAccount(name) {
+    let bool = confirm("Do you want to delete this?");
+    if (bool) {
+        try {
+            storage.deleteAccount(name);
+            remote.getCurrentWindow().reload();
+        }
+        catch (e) {
+            alert(e.message);
+        }
+    }
+}
+
+
+function makeDialog(headerText, bodyObject, listOfButtons) {
+    let dialog = document.getElementById("dialog");
+    let header = document.createElement('h3');
+    header.className += ' mdl-dialog__title';
+    header.innerHTML = headerText;
+    let body = document.createElement('div');
+    body.className += ' mdl-dialog__content';
+    body.appendChild(bodyObject);
+    let actions = document.createElement('div');
+    body.className += ' mdl-dialog__actions';
+
+    for(let i = listOfButtons.length -1 ; i >= 0; i--) {
+        actions.appendChild(listOfButtons[i]);
+    }
+
+    dialog.appendChild(header);
+    dialog.appendChild(body);
+    dialog.appendChild(actions);
+    dialog.showModal();
 }
